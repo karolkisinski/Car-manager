f4446fd22e963ff286262ec86ab14560fde65bfc
karolkisinski <karol.kisinski@wp.pl>
2021-10-21 19:47:35 +0200

    added template for welcome page

--- car_manager/website/views.py
+++ car_manager/website/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from datetime import datetime
 
 def welcome(request):
-    return HttpResponse("Hello")
-
+    return render(request, "website/welcome.html")
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
