ac03a1f154822371a673685f01d35760be948258
karolkisinski <karol.kisinski@wp.pl>
2021-10-21 19:10:38 +0200

    updated car model, added driver model

--- car_manager/cars/models.py
+++ car_manager/cars/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
+class Driver(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Driver {self.first_name} {self.last_name}"
+
 class Car(models.Model):
     brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
-    date = models.DateField()
+    overview_date = models.DateField()
+    oil_change_date = models.DateField()
+    driver= models.ForeignKey(Driver, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.brand} - {self.model}"
+
