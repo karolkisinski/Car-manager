639e0bf57a352cf863d143f14a5ebe35d76e1c71
karolkisinski <karol.kisinski@wp.pl>
2022-03-03 16:15:27 +0100

    Updated date fields to be a date fields

--- car_manager/cars/forms.py
+++ car_manager/cars/forms.py
@@ -4,37 +4,36 @@ from django import forms
 from django.forms import ModelForm
 from .models import Car, Driver
 
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
 class CarForm(ModelForm):
-
-
     class Meta:
         model = Car
         fields = ['brand', 'model', 'overview_date', 'oil_change_date', 'driver']
-    brand = forms.CharField()
-    model = forms.CharField()
-    overview_date = forms.DateInput()
-    oil_change_date = forms.DateInput()
-    # driver = CustomModelMultipleChoiceField(
-    #     queryset=Driver.objects.all(),
-    #     widget=forms.CheckboxSelectMultiple
-    # )
+
     def __init__(self, *args, **kwargs):
         user_id = kwargs.pop('user_id', None)
         super(CarForm, self).__init__(*args, **kwargs)
         self.fields['driver'].queryset = Driver.objects.none()
+        self.fields['overview_date'] = forms.CharField(
+            widget=forms.DateInput(attrs={'type': 'date'}))
+        self.fields['oil_change_date'] = forms.CharField(
+            widget=forms.DateInput(attrs={'type': 'date'}))
         if user_id is not None:
             # update queryset for exercise field
             self.fields['driver'].queryset = Driver.objects.filter(user_id=user_id)
-        #self.fields['driver'] = forms.ModelChoiceField(queryset=Driver.objects.filter(user_id=user_id))
+        # self.fields['driver'] = forms.ModelChoiceField(queryset=Driver.objects.filter(user_id=user_id))
         else:
-            #UserExercises.objects.none() will return an empty queryset
+            # UserExercises.objects.none() will return an empty queryset
             self.fields['driver'].queryset = Driver.objects.none()
-    #driver = forms.ModelChoiceField(queryset=Driver.objects.none())
+    # driver = forms.ModelChoiceField(queryset=Driver.objects.none())
+
+
 class DriverForm(ModelForm):
     class Meta:
         model = Driver
